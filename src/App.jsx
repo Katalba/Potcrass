1dc5eb28b87901947cbeb8d782574c7bb8052ba3
katalba <katherine.albanicg@gmail.com>
2023-07-28 00:10:10 -0300

    Categorias responsive, falta centrar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
+import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
 const App = () => {
   return (
     <>
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="categorias" element={<ItemListContainer/>} />
     </Routes> 
     </>
   );
---
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+
+
+const ItemListContainer = () => {
+  return (
+    <div>
+      <h1>categorias</h1>
+    </div>
+  );
+}
+
+export default ItemListContainer;
