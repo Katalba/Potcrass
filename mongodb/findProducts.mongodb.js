ffaceaa2038972746811d9b25327bcff8c2024fe
katalba <katherine.albanicg@gmail.com>
2023-07-30 15:07:19 -0300

    creando db con mongoDB

---
+++ mongodb/findProducts.mongodb.js
@@ -0,0 +1,14 @@
+use('potcrassdb')
+
+// db.products.insertOne({
+//     name: 'Aloe Cosmos',
+//     category: 'cactus',
+//     price: '1500',
+//     image: 'https://firebasestorage.googleapis.com/v0/b/cactusproject-54bf0.appspot.com/o/aloeComos.jpg?alt=media&token=feb66a41-c982-409c-a8ed-25bcb2180e25',
+//     imageTwo: 'https://firebasestorage.googleapis.com/v0/b/cactusproject-54bf0.appspot.com/o/aloeCosmos-2.jpg?alt=media&token=b526b2c9-2aa2-4bca-b4c1-d85f67442e83',
+//     stock: '20'
+//   })
+
+  db.products.find({})
+
+
